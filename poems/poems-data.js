cba20b885d46cb4e6f2dfd9b4c57732da932b718
Banhpro <thanhkietnguyen5@gmail.com>
2026-03-03 11:17:49 +0700

    poem

---
+++ poems/poems-data.js
@@ -0,0 +1,15 @@
+const poems = [
+  {
+    title: "Về Quê",
+    file: "poems/ve-que.html",
+    theloai: "Trữ tình",
+    thetho: "Lục bát"
+  },
+  {
+    title: "Mưa Chiều",
+    file: "poems/me-gia.html",
+    theloai: "Buồn",
+    thetho: "Tự do"
+  }
+  // thêm tới 100 bài
+];
