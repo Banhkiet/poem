a9fec889da5ec9e480e77e2880b3b796495a3aca
Banhpro <thanhkietnguyen5@gmail.com>
2026-03-03 14:59:42 +0700

    poem

--- poems/poems-data.js
+++ poems/poems-data.js
@@ -1,8 +1,8 @@
 const poems = [
 {
     title: "Mẹ Già",
-    file: "me-gia.html",
-    image: "images/me-gia.jpg",
+    file: "me-gia.html",          // vì nó cùng thư mục
+    image: "images/me-gia.jpg",   // vì images nằm trong poems/
     date: "12/05/2023",
     author: "Bành",
     category: "GIA ĐÌNH",
